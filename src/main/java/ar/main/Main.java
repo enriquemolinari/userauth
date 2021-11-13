7cf4ad001a1949af814b3e455066539975e77b6e
Enrique Pablo Molinari <enrique.molinari@gmail.com>
2021-11-13 09:27:44 -0300

    refactor to constants

--- src/main/java/ar/main/Main.java
+++ src/main/java/ar/main/Main.java
@@ -8,6 +8,9 @@ import ar.model.JwtToken;
 import ar.web.WebAPI;
 
 public class Main {
+  private static final int SECONDS_SINCE_NOW = 6 * 60 * 60 * 1000 /* 6 hs */;
+  private static final String JWT_SECRET = "secret";
+
   public static void main(String[] args) {
 
     EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpa-derby");
@@ -17,7 +20,7 @@ public class Main {
 
     WebAPI servicio = new WebAPI(1234,
         new JpaUserAuth(emf, 
-            new JwtToken("secret", 6 * 60 * 60 * 1000 /* 6 hs */)));
+            new JwtToken(JWT_SECRET, SECONDS_SINCE_NOW)));
     servicio.start();
   }
 }
