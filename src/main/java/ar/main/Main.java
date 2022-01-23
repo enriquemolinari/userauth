21c6387986236c14d23652e182ebc432d66553e9
Enrique Pablo Molinari <enrique.molinari@gmail.com>
2022-01-23 14:19:36 -0300

    Adding customization to extend the token life when using offline first

--- src/main/java/ar/main/Main.java
+++ src/main/java/ar/main/Main.java
@@ -15,21 +15,28 @@ import ar.web.WebAPI;
 
 public class Main {
   private static final int MILLISECONDS_SINCE_NOW = 2 * 60 * 60 * 1000 /* 2 hs */;
+  private static final int OFFLINE_MILLISECONDS_SINCE_NOW = 7 * 60 * 60 * 1000 * 24 /* 7 dias */;
 
   public static void main(String[] args) throws IOException, ParseException {
 
     String tokenSecret = System.getProperty("secret");
-    
+
     if (tokenSecret == null) {
       throw new IllegalArgumentException("secret value must be passed as a jvm argument");
     }
-    
+
     Boolean localTunnel = Boolean.valueOf(System.getProperty("test-with-lt"));
-    
+
     if (localTunnel == null) {
       localTunnel = false;
-    }    
-    
+    }
+
+    Boolean offlineFirst = Boolean.valueOf(System.getProperty("offline"));
+
+    if (offlineFirst == null) {
+      offlineFirst = false;
+    }
+
     EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpa-derby");
 
     SetUpDb setUp = new SetUpDb(emf);
@@ -37,8 +44,11 @@ public class Main {
 
     UserAgentParser parser = new UserAgentService().loadParser();
 
+    int expirationMilliseconds = (offlineFirst) ? OFFLINE_MILLISECONDS_SINCE_NOW
+        : MILLISECONDS_SINCE_NOW;
+
     WebAPI servicio = new WebAPI(1234, localTunnel,
-        new JpaUserAuth(emf, new PasetoToken(tokenSecret, MILLISECONDS_SINCE_NOW)), parser);
+        new JpaUserAuth(emf, new PasetoToken(tokenSecret, expirationMilliseconds)), parser);
     servicio.start();
   }
 }
